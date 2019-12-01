afd82c108d16389f22fa3314587a60560ed866f8
ameer <ameer@ameer-pc.nc.rr.com>
2019-12-01 00:12:37 -0500

    Put main method of game into invokeLater method.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 import model.FastGameOfLifeModel;
 import viewAndController.GameOfLifeController;
 import viewAndController.GameOfLifeView;
@@ -8,19 +10,23 @@ import viewAndController.GameOfLifeView;
 public class Main {
 
 	public static void main(String[] args) {
-		FastGameOfLifeModel model = new FastGameOfLifeModel();
-		GameOfLifeView view = new GameOfLifeView(model);
-		new GameOfLifeController(model, view);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				FastGameOfLifeModel model = new FastGameOfLifeModel();
+				GameOfLifeView view = new GameOfLifeView(model);
+				new GameOfLifeController(model, view);
 
-		// Create top level window.
-		JFrame main_frame = new JFrame();
-		main_frame.setTitle("MVC Conway's Game of Life");
-		main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				// Create top level window.
+				JFrame main_frame = new JFrame();
+				main_frame.setTitle("MVC Conway's Game of Life");
+				main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		main_frame.setContentPane(view);
+				main_frame.setContentPane(view);
 
-		main_frame.pack();
-		main_frame.setVisible(true);
+				main_frame.pack();
+				main_frame.setVisible(true);
+			}
+		});
 	}
 
 }
